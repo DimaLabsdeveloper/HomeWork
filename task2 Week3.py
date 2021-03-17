83650a2e817bfafc8bbceb29a7c585f311df3dd2
Dima <66136912+DimaLabsdeveloper@users.noreply.github.com>
2021-03-17 18:35:23 +0200

    Add files via upload

--- task2 Week3.py
+++ task2 Week3.py
@@ -1,19 +1,24 @@
 import random
 n = random.randint(0,10)
 list = []
+
 for x in range(0,n):
     list.append(random.randint(0,10))
-if n == 0:
+print(list)
+if n == 0 :
         print("число n дорівнює 0 ")
+elif n == 1:
+    print("Найбільший елемент",list[n-1])
 else:
+    
     for x in range(0,n):
         if x == 0:
-            if list[0] > list[1]:
-                print("число",list[0]," більше за",list[1])
-            elif list[0] == list[1]:
-                print("число",list[0]," дорівнює ",list[1])
-            elif list[0] < list[1]:
-                print("число",list[0]," менше за ",list[1])
+            if list[x] > list[x+1]:
+                print("число",list[x]," більше за",list[x+1])
+            elif list[x] == list[x+1]:
+                print("число",list[x]," дорівнює ",list[x+1])
+            elif list[x] < list[x+1]:
+                print("число",list[x]," менше за ",list[x+1])
 
         elif x == n-1:
             if list[n-1] > list[n-2]:
@@ -22,18 +27,12 @@ else:
                 print("число",list[n-1]," дорівнює ",list[n-2])
             elif list[n-1] < list[n-2]:
                 print("число",list[n-1]," менше за ",list[n-2])
-        
-        if list[n] > list[n-1]:
-            print("число",list[n]," більше за",list[n-1])
-        elif list[n] == list[n]:
-            print("число",list[n]," дорівнює ",list[n-1])
-        elif list[n] < list[n-1]:
-            print("число",list[n]," менше за ",list[n-1])
-        elif list[n] > list[n+1]:
-            print("число",list[n]," більше за",list[n+1])
-        elif list[n] == list[n+1]:
-            print("число",list[n+1]," дорівнює ",list[n+1])
-        elif list[n] < list[n+1]:
-            print("число",list[n]," менше за ",list[n+1])
+        else:
+            maxValeu = list[x] 
+            if list[x] < list[x-1]:
+                maxValeu = list[x-1]
+            elif maxValeu < list[x+1] :
+                maxValeu = list[x+1]
+            print ("Найбільший елемент",maxValeu)
 
     
--- task3 Week3.py
+++ task3 Week3.py
@@ -1,4 +1,17 @@
 import random
+list1 =[]
+list2 = []
 n = random.randint(0,10)
 for x in range(0,n):
-    list.append(random.randint(0,10))
+    list1.append(random.randint(0,10))
+m = random.randint(0,10)
+for x in range(0,m):
+    list2.append(random.randint(0,10))
+print(list1)
+print(list2)
+for x in range(0,n):
+    list2.count(list1[x])
+    if list2.count(list1[x]) > 0:
+        print("елемент",list1[x],"присутній в другому списку")
+    else:
+        print("елемент",list1[x],"не присутній в другому списку")
